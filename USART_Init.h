a1b2cad9a9a5fea42f9428e2445db3e1109980fd
jpaine126 <41339610+jpaine126@users.noreply.github.com>
2020-05-28 17:06:51 -0400

    Update USART_Init.h

--- USART_Init.h
+++ USART_Init.h
@@ -1,3 +1,4 @@
+
 /*
  * USART_Init.h
  *
@@ -8,19 +9,24 @@
 
 #include <avr/io.h>
 
-void USART_Init(unsigned int ubrr)
+void USART_Init(void)
 {
-	//UBRR0H = (unsigned char)(ubrr>>8);
-	//UBRR0L = (unsigned char)(ubrr);
-
+	//set baud
 	UBRR0H = UBRRH_VALUE;
     UBRR0L = UBRRL_VALUE;
 
-    //UCSR0B = (1 << RXEN0)|(1 << TXEN0);
-    //UCSR0C = (1 << UCSZ00)|(1 << UCSZ01);
+    //2X mode
+#if USE_2X
+    UCSR0A |= _BV(U2X0);
+#else
+    UCSR0A &= ~(_BV(U2X0));
+#endif
 
+    //enable rx and tx
     UCSR0B = _BV(RXEN0) | _BV(TXEN0);
-    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
+
+    //set frame format: 8data, 2stop
+    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00) | _BV(USBS0);
 }
 
 void USART_transmit_char(unsigned char data)
@@ -43,11 +49,3 @@ void USART_Recieve(void)
 	while( !(UCSR0A & (1 << RXC0) ));
 	return UDR0;
 }
-
-
-
-
-
-
-
-
